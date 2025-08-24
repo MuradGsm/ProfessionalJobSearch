728ad35d1b7b38b597262df29f3e0d98d357efbe
Murad <seferov.murad.98@gmail.com>
2025-08-24 18:29:45 +0400

    'add_jwt_security_and_setting_func'

---
+++ app/auth/deps.py
@@ -0,0 +1,22 @@
+from fastapi import Depends, HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordBearer
+from app.db.fake_db import users_db
+from app.schemas.user_schema import UserResponse
+from app.auth.jwt import decode_access_token
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/auth/login')
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)) -> UserResponse:
+    try:
+        user_id = int(decode_access_token(token))
+        for user in users_db:
+            if user.id == user_id:
+                return user
+        raise HTTPException(status_code=404, detail='User not found')
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid token')
+
+async def admin_required(current_user: UserResponse = Depends(get_current_user)):
+    if not current_user.is_admin:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Admin access required')
---
+++ app/auth/jwt.py
@@ -0,0 +1,36 @@
+from fastapi import HTTPException
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
+from app.config.setting import setting
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=setting.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire, "sub": str(data.get("sub"))})
+    return jwt.encode(to_encode, setting.JWT_SECRET_KEY, algorithm=setting.JWT_ALGORITHM)
+
+def decode_access_token(token: str) -> int:
+    try:
+        payload = jwt.decode(token, setting.JWT_SECRET_KEY, algorithms=[setting.JWT_ALGORITHM])
+        user_id = payload.get("sub")
+        if user_id is None:
+            raise ValueError("Token payload missing user_id")
+        return int(user_id)
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid or expired access token")
+
+def create_refresh_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(days=7)
+    to_encode.update({"exp": expire, "sub": str(data.get("sub"))})
+    return jwt.encode(to_encode, setting.JWT_SECRET_KEY, algorithm=setting.JWT_ALGORITHM)
+
+def decode_refresh_token(token: str) -> int:
+    try:
+        payload = jwt.decode(token, setting.JWT_SECRET_KEY, algorithms=[setting.JWT_ALGORITHM])
+        user_id = payload.get("sub")
+        if user_id is None:
+            raise ValueError("Token payload missing user_id")
+        return int(user_id)
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid or expired refresh token")
