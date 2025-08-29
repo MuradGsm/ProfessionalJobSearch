cdbe195e1c68c57ecfbe437faec194a7c0090683
Murad <seferov.murad.98@gmail.com>
2025-08-29 15:18:24 +0400

    'Add_database_CONFIGURATION

---
+++ app/db/database.py
@@ -0,0 +1,28 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, declared_attr
+from sqlalchemy import func
+from typing import Annotated
+from datetime import datetime
+from app.config import settings
+
+
+engine = create_async_engine()
+async_session  = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
+
+pk_int = Annotated[int, mapped_column(primary_key=True)]
+created_at = Annotated[datetime, mapped_column(default=func.now())]
+updated_at = Annotated[datetime, mapped_column(default=func.now(), onupdate=func.now())]
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f'{cls.__name__.lower()}'
+    
+    created_at: Mapped[created_at]
+    updated_at: Mapped[updated_at]
